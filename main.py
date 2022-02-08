ef47447f04dd5859abd2d3a9945d396164e419d1
Farrukh <fsafarov34@gmail.com>
2022-02-08 22:40:21 +0200

    changed database url DATABASE_URL1 to POSTGRES_DATABASE_URL

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("POSTGRES_DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
